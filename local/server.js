c0dc7cbcc0b584f4498d51777daf334ade98d3ab
Jose Manuel Saborido Noriega <hola@josemasaborido.es>
2026-01-09 12:38:29 +0100

    Agregar registros de configuración .env para parámetros de conexión MQTT

--- local/server.js
+++ local/server.js
@@ -5,6 +5,15 @@ import { handleClientConnection } from './lib/client.js';
 
 dotenv.config();
 
+console.log('Configuración .env:');
+console.log('R3NET_MQTT_URL:', process.env.R3NET_MQTT_URL);
+console.log('R3NET_MQTT_USER:', process.env.R3NET_MQTT_USER);
+console.log('R3NET_MQTT_PASS:', process.env.R3NET_MQTT_PASS);
+console.log('R3NET_WS_PORT:', process.env.R3NET_WS_PORT);
+console.log('R3NET_NODE_ID:', process.env.R3NET_NODE_ID);
+console.log('R3NET_REGION:', process.env.R3NET_REGION);
+console.log('R3NET_CLIENT_TIMEOUT:', process.env.R3NET_CLIENT_TIMEOUT);
+
 const WS_PORT = process.env.R3NET_WS_PORT || 8082;
 
 console.log('Iniciando r3-hub local R3Net...');
